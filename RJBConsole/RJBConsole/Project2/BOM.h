6efa9a1e91c814fe84189aef80ea1ce157bdc894
Robert Beckwith <rjbeckwith55@gmail.com>
2017-11-02 00:23:07 -0500

    added files for project 2 bill of materials

---
+++ RJBConsole/RJBConsole/Project2/BOM.h
@@ -0,0 +1,12 @@
+/*Author: Robert Beckwith
+Date: 11/2/17
+File: BOM.h
+Class: CIS 2542
+Descr:
+	Specification file for the BOM class*/
+
+class BOM {
+public:
+	BOM(); // default constructor
+
+};
