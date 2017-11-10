381dc75f4d6ca75b564743da3fe263b54a5d9bac
Robert Beckwith <rjbeckwith55@gmail.com>
2017-11-10 00:11:33 -0600

    First version of the code before testing it.

--- RJBConsole/RJBConsole/expression_tree.h
+++ RJBConsole/RJBConsole/expression_tree.h
@@ -7,17 +7,20 @@ Descr:
 	Specification file for expression tree which will evaluate postfix operators with a tree
 */
 #include <iostream>
+#include <stack>
 using namespace std;
 
 class expression_tree {
 private:
 	exp_node* root;
+	stack<int> numbers;
 public:
+	//constructors
 	expression_tree :: expression_tree();
 	expression_tree::expression_tree(istream&);
 	expression_tree::~expression_tree(); // destructor
 	
-	int evaluate(exp_node);
+	int evaluate(exp_node*);
 
 	exp_node* get_root() {
 		return root;
@@ -29,7 +32,16 @@ struct exp_node {
 	char op;
 	exp_node* left_field;
 	exp_node* right_field;
+
+	exp_node() { // default values
+		number = 0;
+		op = ' ';
+		left_field = nullptr;
+		right_field = nullptr;
+
+	}
 };
+
 
 // implementation
 
@@ -38,25 +50,79 @@ expression_tree::expression_tree() {
 }
 
 expression_tree::expression_tree(istream& ins) {
-	exp_node* root = new exp_node();
+	exp_node *cursor = get_root();
+	bool done = false;
+	if (get_root() == nullptr) // maybe change with root op and number == ' ' and == 0 respectively
+	{
+		exp_node* root = new exp_node(); // create a new node if there isn't one already
+	}
+
 	int number;
 	char op;
-	if (isdigit(ins.peek())) {
+	bool secondNode = false;
+	do {
 		
-		ins >> number;
-		//recursively populate
-		numbers.push(number); // REPLACE: with Tree
-	}
-	else if (ins.peek() == ADDITION || ins.peek() == DIVISION || ins.peek() == MULTIPLICATION || ins.peek() == SUBRACTION)
-	{
-		ins >> op; // read in the symbol add it to the stack then evaluate
-		operations.push(op); //REPLACE: with Tree
-		expression += symbol; //append the data to the back of the expression for printing
-		evaluate_stack_tops(numbers, operations);
-	}
-	else
-		ins.ignore();
-}
-int expression_tree::evaluate(exp_node root) { // recursively traverse the tree to evaluate it.
+		//check if it is a digit
+		if (isdigit(ins.peek())) {
+			if (secondNode) {
+				// create a new node to move up the tree
+				exp_node* temp = root;
+				exp_node* root = new exp_node();
+				root->left_field = temp;
+			}
+			ins >> number;
+			//populate left side if there isn't anything on the left side
+			if (root->left_field == nullptr) {
+				root->left_field = new exp_node();
+				root->left_field->number = number;
+			}
+			else // populate the right side
+			{
+				root->right_field = new exp_node();
+				root->right_field->number = number;
+			}
 
+		}
+		else if (ins.peek() == '*' || ins.peek() == '+')
+		{
+			ins >> op; // read in the symbol and add it as the root node.
+			root->op = op;
+			secondNode = true; //Set this flag to true so the program knows to creat a new subtree
+		}
+		else
+			ins.ignore();
+	} while (ins);
+}
+
+int expression_tree::evaluate(exp_node* rt) { // recursively traverse the tree to evaluate it.
+	int total;
+	int number;
+	int num1;
+	int num2;
+	if (rt != nullptr) { 
+		//postorder recursively traverse the list
+		total = evaluate(rt->left_field);
+		total = evaluate(rt->right_field);
+
+		//evaluate the one node with two links of the expression.
+		if (rt->op == ' ') {
+			numbers.push(rt->number);
+		}
+		else if (rt->op == '*') {
+			num1 = numbers.top();
+			numbers.pop();
+			num2 = numbers.top();
+			numbers.pop();
+			total += num1 * num2;
+		}
+		else if (rt->op == '+') {
+			num1 = numbers.top();
+			numbers.pop();
+			num2 = numbers.top();
+			numbers.pop();
+			total += num1 + num2;
+		}
+		return total;
+	}
+	
 }
