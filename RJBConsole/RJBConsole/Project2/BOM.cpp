960f33e0ab9b6f3ac7749ae937c9e66e081c92c6
Robert Beckwith <rjbeckwith55@gmail.com>
2017-11-19 13:31:38 -0600

    added a Json library to parse the Json data

--- RJBConsole/RJBConsole/Project2/BOM.cpp
+++ RJBConsole/RJBConsole/Project2/BOM.cpp
@@ -6,9 +6,21 @@ Descr:
 	Implementation file for the BOM class*/
 
 #include "BOM.h"
+#include "json\src\json.hpp" // library from https://github.com/nlohmann/json to parse the JSON data from the spreadsheet
+
+//using json = nlohmann::json;
 
 //TODO: This will automatically intitialize the bill of materials from the spreadsheet sorted into a certain order
 //populates the bill of materials tree with the spreadsheet data
 BOM::BOM() { //default constructor
 	Material* root = new Material();
+
+	ifstream i("file.json");
+	nlohmann::json j;
+	i >> j;
+
+	// write prettified JSON to another file
+	std::ofstream o("pretty.json");
+	o << std::setw(4) << j << std::endl;
+	
 }
